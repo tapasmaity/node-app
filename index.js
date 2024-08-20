633457d43d8911b33e4775dd587e2d57f9a71cd1
Tapasmaity <tapasm@attosol.com>
2024-08-20 23:50:52 +0530

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hello")
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
